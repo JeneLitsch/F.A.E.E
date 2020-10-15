c74a872c31ee405e59dc0578331ab5f504e13835
JeneLitsch <jene.litsch@gmail.com>
2020-10-15 14:41:06 +0200

    Added Copyright Notice

--- FictionalEmulator/main.cpp
+++ FictionalEmulator/main.cpp
@@ -1,3 +1,17 @@
+// Copyright(C) 2012, 2013 Jene Maracel Litsch
+// This file is part of FAEE.
+//
+// FAEE is free software : you can redistribute it and /or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FAEE is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
 #include <iostream>
 #include <fstream>
 #include "Assembler.h"
